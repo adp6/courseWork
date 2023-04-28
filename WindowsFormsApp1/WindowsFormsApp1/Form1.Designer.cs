caeb573f30b60077b9a86677dfcc229d8af5c70f
adp6 <mcdegan244@gmail.com>
2023-04-28 15:09:15 +0300

    initial commit

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelCounters = new System.Windows.Forms.Label();
+            this.countersBox = new System.Windows.Forms.TextBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.White;
+            this.comboBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "genji",
+            "tracer",
+            "reaper",
+            "d.va",
+            "doomfist",
+            "junker queen",
+            "orisa",
+            "reinhardt",
+            "roadhog",
+            "sigma",
+            "winston",
+            "wrecking ball",
+            "zarya",
+            "ashe",
+            "bastion",
+            "cassidy",
+            "echo",
+            "genji",
+            "hanzo",
+            "junkrat",
+            "mei",
+            "pharah",
+            "soujurn",
+            "soldier-76",
+            "sombra",
+            "symmetra",
+            "torbjorn",
+            "widowmaker",
+            "anna",
+            "baptise",
+            "brigitte",
+            "kiriko",
+            "lucio",
+            "mercy",
+            "moira",
+            "zenyatta"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(776, 24);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 38;
+            this.listBox1.Items.AddRange(new object[] {
+            "TYPE: ",
+            "THICKNESS: ",
+            "CROSSHAIR LENGTH:",
+            "CENTER GAP: ",
+            "OPACITY: ",
+            "OUTLINE OPACITY: ",
+            "DOT SIZE:",
+            "DOT OPACITY:"});
+            this.listBox1.Location = new System.Drawing.Point(12, 64);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(505, 308);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.Visible = false;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.ListBox1_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(523, 64);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(265, 308);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.WaitOnLoad = true;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // labelCounters
+            // 
+            this.labelCounters.AutoSize = true;
+            this.labelCounters.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelCounters.Location = new System.Drawing.Point(12, 394);
+            this.labelCounters.Name = "labelCounters";
+            this.labelCounters.Size = new System.Drawing.Size(140, 31);
+            this.labelCounters.TabIndex = 5;
+            this.labelCounters.Text = "Counters: ";
+            this.labelCounters.Visible = false;
+            // 
+            // countersBox
+            // 
+            this.countersBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.countersBox.Location = new System.Drawing.Point(158, 397);
+            this.countersBox.Name = "countersBox";
+            this.countersBox.ReadOnly = true;
+            this.countersBox.Size = new System.Drawing.Size(630, 30);
+            this.countersBox.TabIndex = 6;
+            this.countersBox.Visible = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "genji.png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(804, 460);
+            this.Controls.Add(this.countersBox);
+            this.Controls.Add(this.labelCounters);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Overwatch Crosshair";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelCounters;
+        private System.Windows.Forms.TextBox countersBox;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e) 
+        {
+            hero[] heroArr = { new hero("genji", "crosshairs", 1, 10, 100, 100, 0, 5, 100), new hero("tracer", "crosshairs", 1,5,5,100,100,2,0), new hero("reaper", "circle", 1, 11, 20, 100, 0, 2, 0), new hero("kiriko", "crosshairs", 1,0,0,0,0,5,100), new hero("d.va","circle",3,10,80,100,0,4,100), new hero("roadhog", "circle",1,4,30,100,0,2,0), new hero("soldier-76","crosshairs",1,21,12,100,0,2,0), new hero("orisa","crosshairs",1,10,6,100,0,5,0), new hero("moira","circle",1,10,75,100,0,2,0), new hero("ashe","crosshairs",1,10,0,100,0,2,0), new hero("zenyatta","crosshairs",1,7,6,100,0,2,0), new hero("sigma","crosshairs",1,8,24,100,100,2,0), new hero("cassidy","crosshairs",1,10,10,100,0,2,0), new hero("widowmaker","crosshairs",1,10,0,100,0,2,0), new hero("sombra","circle",1,10,60,100,0,4,100), new hero("torbjorn","circle and crosshairs",1,13,15,100,50,2,0), new hero("zarya","circle",1,0,13,100,0,2,100), new hero("symmetra","circle",1,0,13,100,0,2,100), new hero("mei","crosshairs",2,8,0,100,0,2,100),new hero("pharah","circle",1,0,13,100,0,2,100), new hero("lucio","circle",1,4,25,100,0,2,0), new hero("hanzo","circle",1,15,8,100,0,6,100), new hero("sojourn", "crosshairs",1,10,10,100,0,2,0),new hero("doomfist","circle",1,0,30,100,100,2,100), new hero("junker queen","circle and crosshair",1,6,20,50,50,2,100), new hero("reinhardt","crosshairs",1,10,5,100,50,8,0), new hero("winston","crosshairs",1,7,7,100,100,2,0), new hero("wrecking ball", "crosshairs",1,7,7,100,100,2,0), new hero("bastion","crosshairs",1,7,7,100,100,2,0), new hero("echo", "crosshairs", 1, 7, 7, 100, 100, 2, 0), new hero("baptise", "crosshairs", 1, 7, 7, 100, 100, 2, 0), new hero("mercy","circle",1,0,10,100,100,2,0), new hero("anna","crosshairs",1,7,7,100,100,2,0) };
+
+            for (var i = 0; i < heroArr.Length; i++)
+            {
+                if (comboBox1.Text == heroArr[i].name)
+                {
+                    listBox1.Items[0] = $"TYPE: {heroArr[i].type.ToString()}";
+                    listBox1.Items[1] = $"THICKNESS: {heroArr[i].thickness.ToString()}";
+                    listBox1.Items[2] = $"CROSSHAIR LENGTH: {heroArr[i].crosshairLenght.ToString()}";
+                    listBox1.Items[3] = $"CENTER GAP: {heroArr[i].centerGap.ToString()}";
+                    listBox1.Items[4] = $"OPACITY: {heroArr[i].oppacity.ToString()}%";
+                    listBox1.Items[5] = $"OUTLINE OPACITY: {heroArr[i].outlineOppacity.ToString()}%";
+                    listBox1.Items[6] = $"DOT SIZE: {heroArr[i].dotSize.ToString()}";
+                    listBox1.Items[7] = $"DOT OPPACITY: {heroArr[i].dotOppacity.ToString()}%";
+
+
+                    pictureBox1.BackgroundImage = Image.FromFile($@"D:\heroes png\{heroArr[i].name}Crosshair.jpg");
+
+
+                    listBox1.Visible = true;
+                    countersBox.Text = heroArr[i].counters();
+                    countersBox.Visible = true;
+                    labelCounters.Visible = true;
+                    break;
+                }
+                
+            }
+            
+        }
+
+        public class hero
+        {
+            public string name;
+            public string type;
+            public int thickness;
+            public int crosshairLenght;
+            public int centerGap;
+            public int oppacity;
+            public int outlineOppacity;
+            public int dotSize;
+            public int dotOppacity;
+
+            public hero(string name, string type,int thickness, int crosshairLenght, int centerGap, int oppacity, int outlineOppacity, int dotSize, int dotOppacity)
+            {
+                this.name = name;
+                this.type = type;
+                this.thickness = thickness;
+                this.crosshairLenght = crosshairLenght;
+                this.centerGap = centerGap;
+                this.oppacity = oppacity;
+                this.outlineOppacity = outlineOppacity;
+                this.dotSize = dotSize;
+                this.dotOppacity = dotOppacity;
+
+            }
+            public string counters()
+            {
+                switch (name)
+                {
+                    case "genji":
+                        return "Pharah, Brigitte, Zarya, Anna";
+                    case "reaper":
+                        return "Widowmaker, Hanzo, Soujurn, Soldier-76";
+                    case "tracer":
+                        return "Winston, Brigitte, Pharah, Tourbjorn";
+                    case "hanzo":
+                        return "Widowmaker, Winston, Genji, D.VA";
+                    case "junkrat":
+                        return "Pharah, Kiriko, Echo, Widowmaker";
+                    case "d.va":
+                        return "Sombra, Zarya, Roadhog, Junkrat";
+                    case "doomfist":
+                        return "Reaper, Pharah, Genji, Echo";
+                    case "junker queen":
+                        return "Bastion, Zenyatta, Mei, Orisa";
+                    case "reinhardt":
+                        return "Reaper, Bastion, Pharah, Junkrat";
+                    case "roadhog":
+                        return "Anna, Zarya, Widowmaker, Reaper";
+                    case "sigma":
+                        return "Orisa, Reaper, Genji, Reinhardt";
+                    case "winston":
+                        return "Roadhog, Anna, Junkrat, Moira";
+                    case "wrecking ball":
+                        return "Sombra, Reaper, Bastion, Pharah";
+                    case "zarya":
+                        return "Pharah, Sombra, Zenyatta, Bastion";
+                    case "ashe":
+                        return "Widowmaker, Hanzo, Tracer, Reinhardt";
+                    case "bastion":
+                        return "Roadhog, Junkrat, Anna, Reaper";
+                    case "casidy":
+                        return "Widowmaker, Winston, Baptise";
+                    case "echo":
+                        return "Widowmaker, Soujurn, Hanzo, Sombra";
+                    case "mei":
+                        return "Pharah, Sombra, Widowmaker, Soldier-76";
+                    case "pharah":
+                        return "Widowmaker, Hanzo, Soujurn, Soldier-76";
+                    case "soujurn":
+                        return "Winston, Zarya, Widowmaker, Lucio";
+                    case "soldier-76":
+                        return "Roadhog, Winston, Genji, Sigma";
+                    case "sombra":
+                        return "Hanzo, Junkrat, Widowmaker, Tracer";
+                    case "symmetra":
+                        return "D.VA, Pharah, Winston, Junker queen";
+                    case "torbjorn":
+                        return "Pharah, Widowmaker, Anna, Echo";
+                    case "widowmaker":
+                        return "Winston, Wrecking Ball, Soujurn, Reinhardt";
+                    case "baptise":
+                        return "D.VA, Genji, Hanzo, Roadhog";
+                    case "brigitte":
+                        return "Bastion, Pharah, Junkrat, Hanzo";
+                    case "kiriko":
+                        return "Widowmaker, Sombra, Soujurn, Tracer";
+                    case "lucio":
+                        return "Pharah, Torbjorn, Moira, Bastion";
+                    case "mercy":
+                        return "Widowmaker, Winston, Roadhog, Soujurn";
+                    case "moira":
+                        return "Widowmaker, Sombra, Wrecking Ball, Winston";
+                    case "zenyatta":
+                        return "Junker queen, Winston, Widowmaker, Genji";
+                    case "anna":
+                        return "D.va, Widowmaker, Tracer, Junker Queen";
+                    case "orisa":
+                        return "Winston, Junkrat, Reaper, Zenyatta";
+                    default:
+                        return "none";
+                }
+            }
+
+        }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
